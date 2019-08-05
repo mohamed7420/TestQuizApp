7f7a87893a651983748e50f5ecab17e8af2fe8f6
mohamedosama2074 <iosengks74@gmail.com>
2019-08-05 23:54:15 +0200

    Initial Commit

---
+++ TestQuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestQuizApp
+//
+//  Created by Mohamed on 8/5/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
